55de0916770999bb737e910f2ab45ffe17fcf771
Harsh2534 <121288789+Harsh2534@users.noreply.github.com>
2023-05-05 08:50:56 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -381,8 +381,8 @@ async def get_shortlink(link):
     https = link.split(":")[0]
     if "http" == https:
         https = "https"
-        link = link.replace("mdiskshortner.link", https)
-    url = f'https://{URL_SHORTENR_WEBSITE}/api'
+        link = link.replace("http", https)
+    url = f'https://mdiskshortner/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
